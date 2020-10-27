4c0d098ec827c2890d677ef6bb6878bad09b4804
alesandra mempis <alesandramempis@gmail.com>
2020-10-27 20:26:39 -0300

    While

---
+++ controle/while.js
@@ -0,0 +1,15 @@
+//Quantidade indeterminada de repetições 
+
+function getInteiroAleatorioEntre (min, max){
+    const valor = Math.random() * (max - min) + min;
+    return Math.floor(valor);
+}
+
+let opcao = 0;
+
+while (opcao != -1){ //pode ser verdadeiro ou falso ou executa nenhuma vez ou n de vezes 
+    opcao = getInteiroAleatorioEntre(-1, 10);
+    console.log(`Opção escolhida foi ${opcao}. `);
+}
+
+console.log('Até a próxima');
