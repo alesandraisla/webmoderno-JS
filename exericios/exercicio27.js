d235437951bd9b882e0fe4ae13d241df5e9ed3ff
alesandra mempis <alesandramempis@gmail.com>
2020-11-06 19:56:59 -0300

    Exercicio imprimir o numero máximo e o mínimo de um vetor

---
+++ exericios/exercicio27.js
@@ -0,0 +1,24 @@
+/*
+27) Construa uma função que receba como parâmetros as alturas e as taxas de crescimento anuais de duas
+crianças e calcule se existe uma criança menor, caso exista se a criança menor ultrapassará a maior e em
+quantos anos isso acontecerá. Utilize centímetros para as unidades de medida.
+*/
+
+function diferencaEntreCriancas (crianca1, crianca2){
+
+
+
+}
+
+
+let crianca1 = {
+    altura: 60,
+    taxaDeCrescimento:
+}
+
+let crianca2 = {
+    altura:
+    taxaDeCrescimento:
+}
+
+diferencaEntreCriancas(crianca1, crianca2)
---
+++ exericios/exercicio30.js
@@ -0,0 +1,15 @@
+/*
+30) Escreva um algoritmo que percorre um vetor de inteiros e defina o maior e menor valor dentro do vetor
+*/
+
+    let numeros = [5, 100, 35 ,40, 105]
+
+
+    let maximo = Math.max(...numeros)
+    console.log(maximo)
+
+    let minimo = Math.min(...numeros);
+    console.log(minimo)
+
+
+
