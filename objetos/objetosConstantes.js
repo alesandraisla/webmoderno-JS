8dff3655e412cf1b89c9d2d8d19202fbd1f84f2c
alesandra mempis <alesandramempis@gmail.com>
2020-11-11 20:56:34 -0300

    Objetos constantes

---
+++ objetos/objetosConstantes.js
@@ -0,0 +1,22 @@
+//Não muda a referência da memória 
+// pessoa -> 123 -> {...}
+const pessoa = {nome: 'Lara'}
+console.log(pessoa);
+pessoa.nome = 'Yuna';
+console.log(pessoa)
+
+//nao pode atribuir um novo objeto para uma constante
+//pessoa -> 456 -> {...}
+//pessoa = {nome: 'Ana'}
+
+//congelar um objeto pessoa
+//n conseue mexer mais no objeto (alterar, adicionar, excluir)
+Object.freeze(pessoa)
+pessoa.nome = 'Tidus'
+console.log(pessoa)
+
+
+//nem referência e objeto que foi criado pode ser mudado
+const pessoaConstante = Object.freeze({nome: 'Vanessa'})
+pessoaConstante.nome = 'Beatriz'
+console.log(pessoaConstante)
