eb7ca2fac1623cf25e2baac6cbd62eb3f1351795
alesandra mempis <alesandramempis@gmail.com>
2020-11-21 20:11:49 -0300

    Exercicio retornar uma string com a quantidade de numeros informado como parametro

---
+++ exerciciosProgramacao/10.quantidadeDeNumerosNaString.js
@@ -0,0 +1,13 @@
+/*
+Elabore uma função que recebe um número como parâmetro e retorne uma string com o símbolo "+" na
+quantidade especificada no parâmetro.
+
+*/
+
+function quantidadeDeNumeroNaString(numero){
+
+    return("+".repeat(numero))
+}
+
+console.log(quantidadeDeNumeroNaString(2))
+console.log(quantidadeDeNumeroNaString(4))
