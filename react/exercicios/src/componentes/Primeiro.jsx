1511a4b6ee4efdd75d1034dba4c8b692857cdea0
alesandra mempis <alesandramempis@gmail.com>
2021-01-22 20:32:02 -0300

    react

---
+++ react/exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,7 @@
+// export default function() {
+//     return "Teste2"
+// }
+
+import React from 'react'
+
+export default () => <h1>Primeiro</h1>
---
+++ react/exercicios/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Primeiro from './componentes/Primeiro'
+
+// const elemento = <h1> React2 </h1>
+// ReactDOM.render(elemento, document.getElementById('root'))
+
+ReactDOM.render(<Primeiro />, document.getElementById('root'))
+
+// $('<h1>').html('React 2')
