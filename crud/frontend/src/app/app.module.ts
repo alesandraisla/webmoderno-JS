6828bbb20173530364be7b59e401a8497ac2eb9d
alesandra mempis <alesandramempis@gmail.com>
2021-04-04 16:37:27 -0300

    Componente obter produto

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule  } from '@angular/material/form-field';
 import { MatInputModule  } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 // import { RedDirective } from './directives/red.directive';
 
@@ -35,6 +36,7 @@ import { MatInputModule  } from '@angular/material/input';
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
+    ProductReadComponent,
     // RedDirective,
   ],
   imports: [
