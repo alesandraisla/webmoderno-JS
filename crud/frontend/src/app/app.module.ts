0206a854dc0ea88bbda9eb22ed4aa44d0a148035
alesandra mempis <alesandramempis@gmail.com>
2021-04-04 16:15:19 -0300

    Formulário criar produto

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -18,7 +18,11 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule} from '@angular/material/snack-bar';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule  } from '@angular/material/form-field';
+import { MatInputModule  } from '@angular/material/input';
 
 // import { RedDirective } from './directives/red.directive';
 
@@ -43,7 +47,10 @@ import { HttpClientModule } from '@angular/common/http'
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
