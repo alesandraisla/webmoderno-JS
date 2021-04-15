920773cbd23c95b2619229b835e5c3bf1b932528
alesandra mempis <alesandramempis@gmail.com>
2021-04-15 15:42:38 -0300

    Comando select

---
+++ banco-relacional/consultarEstados.sql
@@ -0,0 +1,23 @@
+select * from estados;
+
+select 
+    Sigla,
+    Nome as 'Nome do Estado'
+from estados
+where regiao = 'Sul';
+
+-- Sem o 'as' também funciona
+select 
+    Sigla, 
+    Nome 'Nome do Estado'
+from estados
+where regiao = 'Sul';
+
+
+select 
+    nome, 
+    regiao,
+    popualacao from estados
+where populacao >= 10
+order by populacao desc;
+
