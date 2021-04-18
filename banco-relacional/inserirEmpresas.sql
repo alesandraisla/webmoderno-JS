07c24747eaee40d993310931ba2beada57c72ac2
alesandra mempis <alesandramempis@gmail.com>
2021-04-18 19:40:29 -0300

    Relação de M para M

---
+++ banco-relacional/inserirEmpresas.sql
@@ -0,0 +1,7 @@
+INSERT INTO empresas (nome, cnpj)
+VALUES  ('Bradesco', 95694186000132),
+        ('Vale', 64239684000161),
+        ('Cielo', 03057702000108);
+
+
+ALTER TABLE empresas MODIFY cnpj VARCHAR(14);        
